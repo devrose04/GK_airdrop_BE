345296dfc25b20b3708d5ba1bf9d31b6e9c28d78
devrose04 <webdevrose04@gmail.com>
2024-06-20 23:36:34 -0700

    update: domain

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ let globalErrorText = "";
 
 const corsOptions = {
   credentials: false,
-  origin: ["gkairdropbe-production-0746.up.railway.app"]
+  origin: ["https://gkairdropbe-production-0746.up.railway.app"]
 };
 app.use(cors(corsOptions));
 // app.use(cors());
